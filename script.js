32ea251ed2a255222e1c89983fbd796905954b57
Sandra1887 <119573339+Sandra1887@users.noreply.github.com>
2024-02-01 09:27:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const x = 5
 const y = 7
 
-function double(x) { return x * 2 }
+function double (x) { return x * 2 }
 
-console.print('Dubbelt av 5 + dubbelt av 7 är: ' + double(x) + double(y))
+console.print('Dubbelt av 5 är ' + double(x) + double(y))
